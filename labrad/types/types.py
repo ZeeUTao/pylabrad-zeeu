938be00a07afd8b2824123df3a7701c62b5fcf56
zeeutao <zeeutao@163.com>
2020-09-22 17:17:38 +0800

    data_vault bug for open('a+') and long type (0L)
    fix data_vault bug for open('a+') and long type (0L)

--- labrad/types/types.py
+++ labrad/types/types.py
@@ -792,6 +792,7 @@ class TValue(Type):
     def __init__(self, unit=None):
         if isinstance(unit, U.Unit):
             unit = str(unit)
+        
         self.unit = unit
 
     def __str__(self):
